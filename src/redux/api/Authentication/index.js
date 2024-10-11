925d9c39c005dc13c8360c512f2e70efd4854c69
Ibrahim Muradov <136456526+IBHMM@users.noreply.github.com>
2024-10-11 11:03:49 +0400

    sdfds

--- src/redux/api/Authentication/index.js
+++ src/redux/api/Authentication/index.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const RegistrationApi = createApi({
   reducerPath: 'registrationApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:3000/api/v1/user',
+    baseUrl: 'https://movon-server-1.onrender.com/api/v1/user',
     credentials: 'include',
   }),
   endpoints: (builder) => ({
